d6501a5747f2c9bc7e5d5dfb2466393fd77c6702
rhetesi <66908912+rhetesi@users.noreply.github.com>
2025-12-16 23:34:00 +0000

    fix sensor not loading bug by ChatGPT

--- custom_components/tvheadend_epg/sensor.py
+++ custom_components/tvheadend_epg/sensor.py
@@ -1,6 +1,19 @@
 from homeassistant.components.sensor import SensorEntity
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
+from .const import DOMAIN
+
+
+async def async_setup_entry(hass, entry, async_add_entities):
+    """Set up TVHeadend EPG sensor from a config entry."""
+    coordinator = hass.data[DOMAIN][entry.entry_id]
+
+    async_add_entities(
+        [
+            TVHeadendEPGSensor(coordinator),
+        ]
+    )
+
 
 class TVHeadendEPGSensor(CoordinatorEntity, SensorEntity):
     _attr_name = "TVHeadend EPG"
@@ -11,10 +24,12 @@ class TVHeadendEPGSensor(CoordinatorEntity, SensorEntity):
 
     @property
     def native_value(self):
+        """Return number of EPG events."""
         return len(self.coordinator.data)
 
     @property
     def extra_state_attributes(self):
+        """Expose full EPG data for Lovelace card."""
         return {
             "epg": self.coordinator.data,
         }
